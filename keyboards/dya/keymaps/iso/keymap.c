626d359ce7a727692a1190d9d6c88c731729a453
cormoran <cormoran707@gmail.com>
2025-03-16 19:05:54 +0900

    Fix keymap for jis

--- keyboards/dya/keymaps/iso/keymap.c
+++ keyboards/dya/keymaps/iso/keymap.c
@@ -12,41 +12,41 @@ enum layer_names { _DEFAULT, _MOUSE, _SCROLL, _CONFIG };
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // [0] = LAYOUT_iso(
-    //     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-    //     XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX,
-    //     XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    //     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    //     XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    //     XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     //     XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     //     XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX                           XXXXXXX,                                XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
     //                                                           XXXXXXX,      XXXXXXX
     // ),
     [_DEFAULT] = LAYOUT_iso(
         JP_ZKHK, JP_1,    JP_2,    JP_3,    JP_4,    JP_5,    JP_6,         JP_6,    JP_7,    JP_8,    JP_9,    JP_0,    JP_MINS, JP_CIRC, JP_YEN, KC_BSPC,
-        KC_TAB,     JP_Q,    JP_W,    JP_E,    JP_R,    JP_T,    JP_Y,          JP_Y,    JP_U,    JP_I,    JP_O,    JP_P,    JP_AT, JP_LBRC,      KC_ENT,
-        JP_EISU,       JP_A,    JP_S,    JP_D,    JP_F,    JP_G,                    JP_H,    JP_J,    JP_K,    JP_L,    JP_SCLN, JP_COLN, JP_RBRC,
+        KC_TAB,     JP_Q,    JP_W,    JP_E,    JP_R,    JP_T,    JP_Y,          JP_Y,    JP_U,    JP_I,    JP_O,    JP_P,    JP_AT, JP_LBRC,
+        KC_LCTL,       JP_A,    JP_S,    JP_D,    JP_F,    JP_G,                    JP_H,    JP_J,    JP_K,    JP_L,    JP_SCLN, JP_COLN, JP_RBRC,  KC_ENT,
         KC_LSFT,          JP_Z,    JP_X,    JP_C,    JP_V,    JP_B,            JP_B,    JP_N,    JP_M,    JP_COMM, JP_DOT,  JP_SLSH, JP_BSLS, KC_UP,   LT(_CONFIG, KC_ESC),
-        KC_LCTL, KC_LGUI, KC_LALT,       JP_MHEN,  KC_SPC,                           KC_SPC,                                KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT,
+        JP_EISU, KC_LGUI, KC_LALT,       JP_MHEN,  KC_SPC,                           KC_SPC,                                KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT,
                                                                  JP_MHEN,       JP_HENK
     ),
     [_MOUSE] = LAYOUT_iso(
         _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______,       _______, _______, MS_BTN3, _______, _______, _______, _______,         _______,
-        _______,       _______, _______, _______, _______, _______,            OSL(_SCROLL), MS_BTN1, MS_BTN2, MO(_MOUSE), MO(_SCROLL), _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,       _______, _______, MS_BTN3, _______, _______, _______, _______,
+        _______,       _______, _______, _______, _______, _______,            OSL(_SCROLL), MS_BTN1, MS_BTN2, MO(_MOUSE), MO(_SCROLL), _______, _______, _______,
         _______,          _______, _______, _______, _______, _______,         _______, MS_BTN4, MS_BTN5, _______, _______, _______, _______, _______, _______,
         _______, _______, _______,       _______, _______,                          _______,                                _______, _______, _______, _______,
                                                           MO(_SCROLL),      _______
     ),
     [_SCROLL] = LAYOUT_iso(
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX,
-        XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX,    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX,                           XXXXXXX,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                                               XXXXXXX,      XXXXXXX
     ),
     [_CONFIG] = LAYOUT_iso(
         QK_BOOT, UG_TOGG, UG_VALU, UG_VALD, UG_HUEU, UG_HUED, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT,
-        QK_RBT,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      QK_RBT,
-        XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        QK_RBT,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, QK_RBT,
         XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         XXXXXXX,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                XXXXXXX,                          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                                               XXXXXXX,              XXXXXXX
